552eb84f54d31b893b3561a9558fc4dd749ae2fd
Himani78116 <himanibagale@gmail.com>
2026-04-15 21:24:48 +0530

    Improving auth pages

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import { supabase } from '../../lib/supabaseClient'
 import { useRouter } from 'next/navigation'
+import "../../styles/auth.css"
 
 export default function Signup() {
   const [email, setEmail] = useState('')
@@ -23,22 +24,24 @@ export default function Signup() {
   }
 
   return (
-    <div>
-      <h2>Sign Up</h2>
+    <div id="body">
+        <div id="signup">
+          <h2>Sign Up</h2>
 
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-      <button onClick={handleSignup}>Create Account</button>
+          <button onClick={handleSignup}>Create Account</button>
+      </div>
     </div>
   )
 }
