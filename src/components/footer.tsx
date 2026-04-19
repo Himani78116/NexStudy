707611bfd8a723720ce5f96071b993d554685642
Himani78116 <himanibagale@gmail.com>
2026-04-20 00:38:35 +0530

    Added footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+'use client'
+import "../styles/landing.css"
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <div className="footer1"></div>
+            <hr></hr>
+            <p></p>
+        </div>
+    )
+}
+
+export default Footer;
