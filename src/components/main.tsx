3b866cb4ded0791f06c592001db7f072938c4111
Himani78116 <himanibagale@gmail.com>
2026-04-21 00:20:31 +0530

    Enhanced landing page by improving how it works section

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -15,7 +15,7 @@ const Landing = () => {
                 backgroundSize: "cover",
                 backgroundPosition: "center",
                 opacity: 0.9
-                }} className="box1 min-h-screen">
+                }} className="box1 min-h-screen" id="home">
                 <Image src={book} alt="Logo" width={200} height={200} priority id="book" className="animated-text w-48 h-auto"/>
                 <p id="heading" className="animated-text">Score with Confidence</p>
                 <p className="subheading animated-text">Turn confusion into clarity with organized exam-ready notes. Clarity for every topic.</p>
@@ -23,22 +23,26 @@ const Landing = () => {
                 <button id="getstarted" className="animated-text" onClick={() => router.push('/signup')}> Get started </button>
                 <p id="bottomtext">STUDY WITH NEXSTUDY</p>
             </div>
-            <div className="box2">
+            <div className="box2" id="how-it-works">
                 <h2 className="working">How does NexStudy work?</h2>
-                <div className='boxof2'>
-
+                <div className='boxof2 box1of2'>
+                    <div className="cardbox1"></div>
+                    <div className="cardbox2"></div>
                 </div>
-                <div className='boxof2'>
-                    
+                <div className='boxof2 box2of2'>
+                    <div className="cardbox3"></div>
+                    <div className="cardbox4"></div>
                 </div>
-                <div className='boxof2'>
-                    
+                <div className='boxof2 box3of2'>
+                    <div className="cardbox5"></div>
+                    <div className="cardbox6"></div>
                 </div>
-                <div className='boxof2'>
-                    
+                <div className='boxof2 box4of2'>
+                    <div className="cardbox7"></div>
+                    <div className="cardbox8"></div>
                 </div>
             </div>
-            <div className="box3">
+            <div className="box3" id="features">
                 <p className="text1 bg-gradient-to-r from-green-500 to-green-700 bg-clip-text text-transparent">Built for making your preparation easy!</p>
                 <div className="horizontal-section">
                     <div className="scroll-container">
