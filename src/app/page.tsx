860ab975fe2b045d69e98b9686c39674daa01259
Himani78116 <himanibagale@gmail.com>
2026-05-02 23:33:58 +0530

    Added preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,40 @@
+"use client";
 import type { Metadata} from "next";
 import Navbar from "../components/navbar";
 import Landing from "../components/main";
 import Footer from "../components/footer";
+import Preloader from "../components/preloader";
+import {useState, useEffect} from "react";
+import { AnimatePresence } from "framer-motion";
 
-export const metadata: Metadata = {
-  title: "NexStudy",
-  description: "Exam prep notes app",
-};
+// export const metadata: Metadata = {
+//   title: "NexStudy",
+//   description: "Exam prep notes app",
+// };
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // match animation duration
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <Navbar />
-      <Landing />
-      <Footer />
+      <AnimatePresence mode="wait">
+        {loading && <Preloader key="preloader" />}
+      </AnimatePresence>
 
+      {!loading && (
+        <>
+          <Navbar />
+          <Landing />
+          <Footer />
+        </>
+      )}
     </>
   )
 }
---
+++ src/components/preloader.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { motion } from "framer-motion";
+import "../styles/landing.css"
+import { Variants } from "framer-motion";
+
+const text = "NexStudy";
+
+const container: Variants = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.12,
+    },
+  },
+};
+
+const letter: Variants = {
+  hidden: { opacity: 0, y: 40 },
+  visible: { opacity: 1, y: 0 },
+};
+
+const Preloader = () => {
+     return (
+    <motion.div
+      className="fixed inset-0 flex items-center justify-center bg-gradient-to-r from-gray-100 to-white z-50"
+      initial="hidden"
+      animate="visible"
+      exit={{
+        opacity: 0,
+        scale: 1.1,
+        transition: { duration: 0.6 }
+    }}
+    >
+      <motion.h1
+        className="text-9xl font-bold text-black flex gap-2 tracking-[0.35em] 
+drop-shadow-[0_4px_10px_rgba(0,0,0,0.3)] 
+[ text-shadow: 0px 2px 2px rgba(255,255,255,0.6), 0px -2px 4px rgba(0,0,0,0.2) ] style"
+        variants={container}
+      >
+        {text.split("").map((char, i) => (
+          <motion.span key={i} variants={letter}>
+            {char}
+          </motion.span>
+        ))}
+      </motion.h1>
+    </motion.div>
+  );
+}
+export default Preloader;
